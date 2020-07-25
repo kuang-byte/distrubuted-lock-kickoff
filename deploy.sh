1ec437a2c840863d880770da9a754c5cb379234c
hangpei <allen.hangpei@gmail.com>
2020-07-25 00:02:33 -0400

    wip

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,19 @@
 
 # pull
 ./mvnw docker-compose:pull@pull
-# up
-./mvnw docker-compose:up@up
-# down
+
+echo "$REDIS_ENV"
+
+if [[ "$REDIS_ENV" == prod ]]
+then
+    echo match
+    ./mvnw docker-compose:up@up
+elif [[ "$REDIS_ENV" == cluster ]]
+then
+    ./mvnw docker-compose:up@up
+elif [[ "$REDIS_ENV" == cluster ]]
+then
+    ./mvnw docker-compose:up@up
+fi
+
 # mvn docker-compose:down@down
